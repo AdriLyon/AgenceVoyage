3b12410a5583f7e2a9d57e0532fa1ac95ad29ecf
Adrien COVAREL <adrien.covarel@tpi13.cpe.fr>
2020-10-20 11:53:00 +0200

    Chnagements 20/10

--- HTML/PremierePage.js
+++ HTML/PremierePage.js
@@ -5,15 +5,18 @@ window.onload = function(e){
 
 class destinations {
     constructor(pays) {
-        this.pays = pays;       
+        this.pays = pays;
+        this.prix = prix;       
     }
 }
 
-var desination1 = new destinations("LeCaire");
-var desination2 = new destinations("Casablanca");
-var desination3 = new destinations("Johannesburg");
-var desination4 = new destinations("Madagascar");
-var desination5 = new destinations("Zimbabwe");
-var desination6 = new destinations("Tanzanie");
+var desination1 = new destinations("LeCaire",100);
+var desination2 = new destinations("Casablanca",100);
+var desination3 = new destinations("Johannesburg",100);
+var desination4 = new destinations("Madagascar",100);
+var desination5 = new destinations("Zimbabwe",100);
+var desination6 = new destinations("Tanzanie",100);
+
 
 let sejour_id = new URLSearchParams(window.location.search).get("id");
+
